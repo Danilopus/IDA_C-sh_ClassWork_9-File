0fc96e1bbe94dd4b7a758d6518d3eda165b24063
С - 7 < - 7@WIN-1B59ROQRL87>
2023-10-18 21:13:24 +0500

    Add project files.

---
+++ IDA_C-sh_ClassWork_9/!_Program.cs
@@ -0,0 +1,100 @@
+﻿// ClassWork template 1.2 // date: 29.09.2023
+
+using IDA_C_sh_ClassWork_9;
+using Service;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+
+
+// ClassWork 09 : [Csharp Filesystem] 18.10.2023 --------------------------------
+
+// 
+/*
+ 
+*/
+
+//
+/**/
+
+//
+/**/
+
+//
+/**/
+
+//
+/**/
+
+namespace IDA_C_sh_ClassWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            Console.WriteLine(arg);
+
+            Directory.GetCurrentDirectory();
+
+            //Task_1();
+             Task_2();
+            // Task_3();
+
+            Console.ReadKey();
+
+        }
+
+        public static void Task_1() 
+        {
+            string path = Path.GetTempFileName();     
+            //Path.Get
+
+            // Through System.IO.File 
+            //FileStream str_1 = File.Create(path);
+            FileStream str_1 = new(path, FileMode.Open);
+
+
+            StreamWriter streamWriter_1 = new StreamWriter(str_1);
+            streamWriter_1.WriteLine("streamWriter_1 string_1");
+            streamWriter_1.WriteLine("streamWriter_1 string_2");
+            streamWriter_1.WriteLine("streamWriter_1 string_3");
+            streamWriter_1.Close();
+
+            //_ = str_1.CanRead;
+            str_1 = new(path, FileMode.OpenOrCreate);
+            //FileStream str_for_read = new(path, FileMode.Open);
+            StreamReader streamReader_3 = new StreamReader(str_1);
+            //StreamReader streamReader_3 = new StreamReader(str_for_read);
+            string read_result = streamReader_3.ReadToEnd();
+            Console.WriteLine(read_result);
+
+
+            // Through System.IO.FileOnfo
+            string path_2 = Path.GetTempFileName();
+            FileInfo new_fileinfo_obj = new FileInfo(path_2);            
+            FileStream str_2 = new_fileinfo_obj.Create();
+            string path_3 = Path.GetTempFileName();
+            FileStream str_3 = new FileInfo(path_3).Create();         
+
+
+        }
+        public static void Task_2() 
+        {
+        try { throw new Exception("test exeption"); }
+        //catch (Exception ex) { new ExceptionHandler(ex, TypeInfo_ = true, FullTypeInfo_ = false, StackTrace_ = false); }
+        catch (Exception ex) 
+            { 
+                new ExceptionHandler(ex, false, false, false, false);
+                new ExceptionHandler(ex);
+            }
+        }
+        public static void Task_3() { }
+
+    } // class Programm
+}// namespace
+
---
+++ IDA_C-sh_ClassWork_9/ExceptionLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDA_C_sh_ClassWork_9
+{
+    internal class ExceptionHandler
+    {
+        public bool TypeInfo_ { get; set; } = true;
+        public bool FullTypeInfo_ { get; set; } = true;
+        public bool StackTrace_ { get; set; } = true;
+        public bool Message_ { get; set; } = true;
+
+        /*   
+            public ExceptionHandler(Exception ex, bool TypeInfo, bool FullTypeInfo,bool StackTrace): this(ex)
+           {
+               this.TypeInfo_ = TypeInfo;
+               this.FullTypeInfo_ = FullTypeInfo;
+               this.StackTrace_ = StackTrace;
+           }//*/
+
+        public ExceptionHandler(Exception ex, bool Message = true, bool TypeInfo = true, bool FullTypeInfo = true, bool StackTrace = true)
+        {
+            this.Message_ = Message;
+            this.TypeInfo_ = TypeInfo;
+            this.FullTypeInfo_ = FullTypeInfo;
+            this.StackTrace_ = StackTrace;
+
+            string log_message = $"\nTimeStamp: {DateTime.Now}";
+                if (Message_) log_message += $"\nMessage: {ex.Message}";
+                if (FullTypeInfo_) log_message += $"\nInfo: {ex.ToString()}";
+                if (TypeInfo_) log_message += $"\nType: {ex.GetType()}";
+                if (StackTrace_) log_message += "\n"+ ex.StackTrace;
+            new FileLogger(log_message);
+            Console.WriteLine(log_message);
+        }
+        class FileLogger
+        {
+        public string Path_ { get; set; }  = Directory.GetCurrentDirectory()+ "\\log_file.log";
+        public FileLogger(string log_message) 
+            { 
+                if (!File.Exists(Path_)) { File.Create(Path_).Close(); }
+                StreamWriter streamWriter = new StreamWriter(Path_, true);
+                //StreamWriter streamWriter = new StreamWriter(Path_);
+                streamWriter.WriteLine(log_message);
+                streamWriter.Close();
+            }
+        }
+    }
+}
